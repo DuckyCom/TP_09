fe14e7ebd3cacafd0a701b830cfc9c78621eb1de
BENJAMIN ROLANDO FELDMAN AGUERO <48116117@ort>
2023-10-09 10:23:45 -0300

    2.1 Finale

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using TP_09.Models;
 //cambiarle tambiene l nombre a la carpeta donde estan los .cshtml (de "Home" a "Account")
 namespace TP_09.Controllers
 {
-    public class HomeController : Controller
+    public class AccountController : Controller
     {
         public IActionResult Index()
         {
@@ -25,8 +25,8 @@ namespace TP_09.Controllers
         {
             bool validacion = BD.UsuarioExiste(us.username);
             if(validacion == true){
-                ViewBag.MensajeError = "Ese nombre de usuario ya existe";
-                return RedirectToAction("ErrorLogin");
+             ViewBag.MensajeError = "Ese nombre de usuario ya existe";
+                return View("ErrorLogin");
             }
             else{
             ViewBag.Usuario = us;
@@ -42,27 +42,22 @@ namespace TP_09.Controllers
         public IActionResult ResultadoLogin(string username, string contraseña)
         {
             ViewBag.Usuario = BD.Login(username, contraseña);
-            bool usuarioEncontrado = false; 
             if(ViewBag.Usuario != null){
-                usuarioEncontrado = true;
+                return View("Bienvenida");
             }
-            if(usuarioEncontrado == true)
-            {   
-            //--- Despues de ingresar correctamente sus datos, ya esta en la view Bienvenida ---
-               return View("Bienvenida");
+            else{
+                //esto no ocurre, tira un error (si te equivocas en contraseña Y/o nombre)
+                //ViewBag.MensajeError = "Nombre de usuario o contraseña inexistente";
+                return View("ErrorLogin");
             }
-            // --- Si los datos que ingreso no coinciden con ninguno de BD, lo manda a una view que le dice que esta mal y de esa view hay un button para ir a login ---
-            else return RedirectToAction("ErrorLogin");
-        }
-        public IActionResult ErrorLogin(){
-            return View();
-        }
+        } 
         
-        public IActionResult NuevaContraseña(string username, string nuevaContraseña, string telefono) //falta hacer que si el usuario y/o email ingresados no se encuentran lo tiene que redireccionar a ErrorLogin
-        { bool validacion = BD.UsuarioExiste(username);
+        public IActionResult NuevaContraseña(string username, string nuevaContraseña, string telefono) 
+        { 
+            bool validacion = BD.UsuarioExiste(username);
             if(validacion == false){
                 ViewBag.MensajeError = "Ese nombre de usuario no existe";
-                return RedirectToAction("ErrorLogin");
+                return View("ErrorLogin");
             }
             else
             {
@@ -71,10 +66,6 @@ namespace TP_09.Controllers
             return View("Login");
             }
         }
-
-        
-
-       
 
         
     }
